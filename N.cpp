4b937faed50b41c3bb65a8bbd6c83dc67fa8223f
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-03-12 15:18:14 +0600

    coversion of char

--- N.cpp
+++ N.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 int main()
 {
-    printf("Niaz");
+    char x;
+    cin>>x;
+    if((int)x>=65 && (int)x<=90)
+    {
+        x= x+32;
+        cout<<x;
+    }
+    else if((int)x>=97 && (int)x<=122)
+    {
+        x = x-32;
+        cout<<x;
+    }
     
 }

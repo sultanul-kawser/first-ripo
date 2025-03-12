831dd40999d563553d830462fbb7d7afdbe5a142
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-03-13 01:54:41 +0600

    codeforce

--- practice problems/X.cpp
+++ practice problems/X.cpp
@@ -1,14 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    long long int a , b , c , d ;
-    cin >> a >> b >> c >> d ;
-    if(a>=c && b>=d)
+    long long int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    if (c >= a && c <= b)
     {
-        if(c>a && d>b)
+        if (b > c && b < d)
         {
-            cout<<(b-c);
+            cout << c << " " << b;
         }
+    }
+    else if (a >= c && a <= d)
+    {
+        if (d > a && d < b)
+        {
+            cout << a << " " << d;
+        }
+    }
+    else if (a == c && b == d)
+    {
+        if (a == c)
+        {
+            cout << a << " " << c;
+        }
+        else if (b == d)
+        {
+            cout << b << " " << d;
+        }
+    }
+    else
+    {
+        cout << "-1";
     }
 }
---
+++ practice problems/power.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a = 3,b = 3,result; 
+     result = pow(a,b);
+    printf("%d",result);
+}

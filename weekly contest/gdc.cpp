1072b8752da17cff884731c39b2d4e276825fd2f
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-03-18 02:40:05 +0600

    contest

--- weekly contest/gdc.cpp
+++ weekly contest/gdc.cpp
@@ -4,14 +4,8 @@ int main()
 {
     int a, b, big, small, storeA[1000];
     cin >> a >> b;
-    
-    // separating the big and small number from the input 
-    // to calculate the divisor of the big number
     big = max(a, b);
     small = min(a, b);
-
-    // finding the divisor of the big number 
-    // and storing them in the storeA array
     int j = 0;
     for (int i = 1; i <= big; i++)
     {
@@ -22,8 +16,6 @@ int main()
         }
     }
 
-    //checking if the divisor of the small number is simillar to the divisor 
-    // of the big number and storing them in the storeB array
     int storeB[1000], l = 0;
     for (int k = 0; k < j; k++)
     {
@@ -33,6 +25,5 @@ int main()
             l++;
         }
     }
-    // printing the last element of the storeB array
     cout << storeB[l - 1] << endl;
 }

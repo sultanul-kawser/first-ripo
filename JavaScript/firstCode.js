e7137e94fe6c094e88362ff137e528eba5ea1bd8
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-04-14 22:15:29 +0600

    javascript

---
+++ JavaScript/firstCode.js
@@ -0,0 +1,8 @@
+console.log("Hello World!")
+console.log("let's assign some value to some variables")
+let name = "Niaz"
+let age = 19
+const gender = "Male"
+console.log(name)
+console.log(age)
+console.log(gender)
---
+++ JavaScript/operators.js
@@ -0,0 +1,6 @@
+let a = 3.43
+let b = 5.23
+console.log(a+=5)
+console.log(a*b)
+console.log(5%3)
+console.log(b-a)

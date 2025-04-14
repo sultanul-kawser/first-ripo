13c02c40058c8342662ac4a5a38fbc16d8db2ea0
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-04-14 14:10:49 +0600

    python

---
+++ python/variables.py
@@ -0,0 +1,18 @@
+# assigning value to a variable 
+name = "Niaz" #python can detect data type automatically
+age = 20
+is_married = False #bool are used to create logic in programme
+is_student = True
+
+# 'None' is a value that tells you that 
+# a variable exists but has no value yet
+is_graduate = None
+is_graduate = True #value will change if we assign a new value to a existing variable
+
+
+print(name)
+print(age)
+print(is_married)
+print(is_student)
+print(is_graduate)
+

51ee0d830ff802bca031820c0e99feb395f275c8
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-05-12 01:46:16 +0600

    codeforces

--- A_Translation.cpp
+++ A_Translation.cpp
@@ -5,22 +5,32 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-     
-    string line , revLine;
-    cin>>line>>revLine ;
+
+    string line, revLine;
+    cin >> line >> revLine;
     int len = line.size();
     bool isSame = true;
-    for(int i = 0; i<line.size(); i++){
-        if(line[i] != revLine[len - i -1]){
-            isSame = false;
-            break;
+    if (line.size() != revLine.size())
+    {
+        cout << "NO";
+    }
+    else
+    {
+        for (int i = 0; i < line.size(); i++)
+        {
+            if (line[i] != revLine[len - i - 1])
+            {
+                isSame = false;
+                break;
+            }
+        }
+        if (isSame)
+        {
+            cout << "YES";
+        }
+        else
+        {
+            cout << "NO";
         }
     }
-    if(isSame){
-        cout<<"YES";
-    }else {
-        cout<<"NO";
-    }
-    
-   
 }

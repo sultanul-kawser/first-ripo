65fd8116ea5b8640fc1ccf5ea642b34d022918a6
sultanul-kawser <niazsultanulkawser@gmail.com>
2025-05-20 22:09:37 +0600

    codeforces

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    long long n;
+    cin >> n;
+    long long lastTwo;
+    for(auto i = 1; i <= n; i++){
+        long long result = 5 * 5;
+        lastTwo = result % 100;
+    }
+    cout << lastTwo;
+    
+    return 0;
+}
